e602e89eb52c840c246ba810b0367be6f5f6a349
ekater1na <ekaterina_l@tut.by>
2020-01-21 18:28:41 +0300

    feat: add page components

--- front-end-app/src/app/app.module.ts
+++ front-end-app/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RegComponent } from './reg/reg.component';
+import { AuthComponent } from './auth/auth.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RegComponent,
+    AuthComponent,
+    DashboardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
